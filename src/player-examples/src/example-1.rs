5da79eb28fc8715f44c1c9a448872d6708084cb3
Petteri Mattila <petterimattila@protonmail.com>
2025-03-25 16:12:07 +0200

    Improvements

---
+++ src/player-examples/src/example-1.rs
@@ -0,0 +1,49 @@
+use gst::prelude::*;
+
+#[path = "../common.rs"]
+mod tutorials_common;
+
+fn tutorial_main() {
+    // Initialize GStreamer
+    gst::init().unwrap();
+
+    // Build the pipeline
+    let uri = "https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm";
+    let pipeline = gst::parse::launch(&format!("playbin uri={uri}")).unwrap();
+
+    // Start playing
+    pipeline
+        .set_state(gst::State::Playing)
+        .expect("Unable to set the pipeline to the `Playing` state");
+
+    // Wait until error or EOS
+    let bus = pipeline.bus().unwrap();
+    for msg in bus.iter_timed(gst::ClockTime::NONE) {
+        use gst::MessageView;
+
+        match msg.view() {
+            MessageView::Eos(..) => break,
+            MessageView::Error(err) => {
+                println!(
+                    "Error from {:?}: {} ({:?})",
+                    err.src().map(|s| s.path_string()),
+                    err.error(),
+                    err.debug()
+                );
+                break;
+            }
+            _ => (),
+        }
+    }
+
+    // Shutdown pipeline
+    pipeline
+        .set_state(gst::State::Null)
+        .expect("Unable to set the pipeline to the `Null` state");
+}
+
+fn main() {
+    // tutorials_common::run is only required to set up the application environment on macOS
+    // (but not necessary in normal Cocoa applications where this is set up automatically)
+    tutorials_common::run(tutorial_main);
+}
