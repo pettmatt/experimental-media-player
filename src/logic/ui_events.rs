4a6337b6d006a808585d5d3beb2b6148c038a247
Petteri Mattila <petterimattila@protonmail.com>
2025-10-30 13:36:58 +0200

    Refactoring

--- src/logic/ui_events.rs
+++ src/logic/ui_events.rs
@@ -39,117 +39,137 @@ pub fn handle_events(app: &AppWindow, state: &mut Rc<RefCell<State>>) {
 	let player = Rc::new(RefCell::new(MediaPlayer::new()));
 	let global_setting_actions = app.global::<SettingActions>();
 	let global_media_actions = app.global::<MediaActions>();
-
-	let mut player_clone_1 = Rc::clone(&player);
-	let mut player_clone_2 = Rc::clone(&player);
-	let mut player_clone_3 = Rc::clone(&player);
-	let mut player_clone_4 = Rc::clone(&player);
-	let mut player_clone_5 = Rc::clone(&player);
-	let mut player_clone_6 = Rc::clone(&player);
-	let player_clone_7 = Rc::clone(&player);
-	
-	let mut state_clone_1 = Rc::clone(state);
-	let mut state_clone_2 = Rc::clone(state);
-	let state_clone_3 = Rc::clone(state);
-	let state_clone_4 = Rc::clone(state);
-	let state_clone_5 = Rc::clone(state);
-	let state_clone_6 = Rc::clone(state);
-	
 	let weak_app = app.as_weak();
-	let app_clone = weak_app.clone();
 
 	// Media elements bottom panel.
-	global_media_actions.on_media_start(move |id: i32| {
-		let temp_state = state_clone_1.borrow().index.clone();
-		if let Some((index, media)) = temp_state.iter().enumerate()
-			.find(|(_, item)| item.id == id) {
-				audio_control_events::handle_media_start(&mut player_clone_1, media);
-				state_clone_1.borrow_mut().add_to_queue(media);
+	global_media_actions.on_media_start({
+		let app_clone = weak_app.clone();
+		let state_clone = Rc::clone(state);
+		let mut player_clone = Rc::clone(&player);
 
-				if let Some(app) = app_clone.upgrade() {
-					state_clone_1.borrow_mut().set_queue(None, &app.global::<SlintState>());
-					state_clone_1.borrow_mut().playing.media_index = Some(index);
+		move |id: i32| {
+			let temp_state = state_clone.borrow().index.clone();
+			if let Some((index, media)) = temp_state.iter().enumerate()
+				.find(|(_, item)| item.id == id) {
+					audio_control_events::handle_media_start(&mut player_clone, media);
+					state_clone.borrow_mut().add_to_queue(media);
 
-					let temp_queue = state_clone_1.borrow().queue.clone();
-					if let Some((queue_index, _)) = temp_queue.iter().enumerate()
-						.find(|(_, queue_item)| queue_item.media_id == media.id) {
-							state_clone_1.borrow_mut().playing.queue_index = Some(queue_index);
-						}
-				}
+					if let Some(app) = app_clone.upgrade() {
+						state_clone.borrow_mut().set_queue(None, &app.global::<SlintState>());
+						state_clone.borrow_mut().playing.media_index = Some(index);
+
+						let temp_queue = state_clone.borrow().queue.clone();
+						if let Some((queue_index, _)) = temp_queue.iter().enumerate()
+							.find(|(_, queue_item)| queue_item.media_id == media.id) {
+								state_clone.borrow_mut().playing.queue_index = Some(queue_index);
+							}
+					}
+			}
 		}
 	});
-	global_media_actions.on_media_change(move |index: i32| {
-		println!("(UI Events) Media changed");
-		let queue_result = state_clone_2
-			.borrow_mut().update_playing_audio_in_queue(index);
+	global_media_actions.on_media_change({
+		let app_clone = weak_app.clone();
+		let state_clone = Rc::clone(state);
+		let mut player_clone = Rc::clone(&player);
 
-		if let Some(app) = app_clone.upgrade() {
-			state_clone_2.borrow_mut().set_queue(None, &app.global::<SlintState>());
-		}
+		move |index: i32| {
+			println!("(UI Events) Media changed");
+			let queue_result = state_clone
+				.borrow_mut().update_playing_audio_in_queue(index);
 
-		if let Some((previous_index, target_index)) = queue_result {
-			let is_empty = state_clone_2.borrow().queue.is_empty();
-			if !is_empty {
-				let id = state_clone_2.borrow().queue[target_index].media_id;
-				if let Some((_, media)) = state_clone_2.borrow().find_source_by_id(id) {
-					audio_control_events::handle_media_change(
-						&mut player_clone_2,
-						media,
-						(previous_index, target_index)
-					);
+			if let Some(app) = app_clone.upgrade() {
+				state_clone.borrow_mut().set_queue(None, &app.global::<SlintState>());
+			}
+
+			if let Some((previous_index, target_index)) = queue_result {
+				let is_empty = state_clone.borrow().queue.is_empty();
+				if !is_empty {
+					let id = state_clone.borrow().queue[target_index].media_id;
+					if let Some((_, media)) = state_clone.borrow().find_source_by_id(id) {
+						audio_control_events::handle_media_change(
+							&mut player_clone,
+							media,
+							(previous_index, target_index)
+						);
+					}
 				}
 			}
 		}
 	});
-	global_media_actions.on_media_toggle(move ||
-		audio_control_events::handle_media_toggle(&mut player_clone_3));
-	global_media_actions.on_media_change_volume(move |volume: i32|
-		audio_control_events::handle_media_volume(&mut player_clone_4, volume));
-	global_media_actions.on_media_change_track_position(move |position| {
-		let duration_position = std::time::Duration::from_secs_f32(position as f32);
-		audio_control_events::change_current_track_position(
-			&mut player_clone_5,
-			duration_position
-		);
+	global_media_actions.on_media_toggle({
+		let mut player_clone = Rc::clone(&player);
+		move || audio_control_events::handle_media_toggle(&mut player_clone)
 	});
-	global_media_actions.on_media_get_track_position(move || {
-		println!("(Event) Get track triggered");
-		let value = audio_control_events::get_current_track_position(& player_clone_6);
-		state_clone_3.borrow_mut().playing.update_position(value as i32);
+	global_media_actions.on_media_change_volume({
+		let mut player_clone = Rc::clone(&player);
+		move |volume: i32|
+			audio_control_events::handle_media_volume(
+				&mut player_clone, volume
+			)
 	});
-	global_media_actions.on_media_mix(move || {
-		state_clone_4.borrow_mut().shuffle();
-		audio_control_events::handle_media_mix(&player_clone_7);
+	global_media_actions.on_media_change_track_position({
+		let mut player_clone = Rc::clone(&player);
+		move |position| {
+			let duration_position = std::time::Duration::from_secs_f32(position as f32);
+			audio_control_events::change_current_track_position(
+				&mut player_clone,
+				duration_position
+			);
+		}
+	});
+	global_media_actions.on_media_get_track_position({
+		let state_clone = Rc::clone(state);
+		let player_clone = Rc::clone(&player);
+
+		move || {
+			println!("(Event) Get track triggered");
+			let value = audio_control_events::get_current_track_position(& player_clone);
+			state_clone.borrow_mut().playing.update_position(value as i32);
+		}
+	});
+	global_media_actions.on_media_mix({
+		let state_clone = Rc::clone(state);
+		let player_clone = Rc::clone(&player);
+
+		move || {
+			state_clone.borrow_mut().shuffle();
+			audio_control_events::handle_media_mix(&player_clone);
+		}
 	});
 
 	// Settings
-	global_setting_actions.on_new_local_source(move || {
-		let source: Option<std::path::PathBuf> = source::new_local_source();
+	global_setting_actions.on_new_local_source({
+		let state_clone = Rc::clone(state);
+		move || {
+			let source: Option<std::path::PathBuf> = source::new_local_source();
+			match source {
+				Some(source) => {
+					{
+						let path_string = source.clone().to_str().unwrap().to_string();
+						database::add_record(Source {
+							origin: String::from("local"),
+							path: path_string
+						});
+					}
 
-		match source {
-			Some(source) => {
-				{
-					let path_string = source.clone().to_str().unwrap().to_string();
-					database::add_record(Source {
-						origin: String::from("local"),
-						path: path_string
-					});
-				}
-
-				println!("Directory fetched correctly {:?}", source);
-				let records = source::read_source(source).expect("Couldn't fetch all files");
-				database::add_records(records.clone());
-				state_clone_5.borrow_mut().merge_to_index(records);
-			},
-			None => println!("Didn't receive a path. Result should be None: {:?}", source)
+					println!("Directory fetched correctly {:?}", source);
+					let records = source::read_source(source).expect("Couldn't fetch all files");
+					database::add_records(records.clone());
+					state_clone.borrow_mut().merge_to_index(records);
+				},
+				None => println!("Didn't receive a path. Result should be None: {:?}", source)
+			}
 		}
 	});
 
-	global_media_actions.on_add_to_playlist(move |playlist_id: i32, media_id: i32| {
+	global_media_actions.on_add_to_playlist({
+		let app_clone = weak_app.clone();
+		let state_clone = Rc::clone(state);
+		move |playlist_id: i32, media_id: i32| {
 		if let Some(app) = app_clone.upgrade() {
-			state_clone_6.borrow_mut().add_playlist(playlist_id, media_id, &app.global::<SlintState>());
+			state_clone.borrow_mut().add_to_playlist(playlist_id, media_id, &app.global::<SlintState>());
 		}
-	});
+	}});
 }
 
 mod audio_control_events {
