0663753be2a579b371b4f7a7522c2895769bb1e1
pettmatt <petterimattila@protonmail.com>
2025-11-21 16:43:49 +0200

    Fixed and expanded to successfull auth request

--- requests/src/main.rs
+++ requests/src/main.rs
@@ -1,68 +1,83 @@
 use serde::Deserialize;
 use serde_json::Value;
-use std::{collections::HashMap, path::Path, str::FromStr};
+use std::env;
+use std::{collections::HashMap, path::Path};
 use tokio;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let apis = fetch_apis("./src/requests.json").unwrap();
+    dotenv::from_filename(".env-requests").ok(); // Load env files
+    let apis = fetch_apis("./src/apis.json").unwrap();
+    println!("APIs {:?}", apis);
 
-    for api in &apis {}
+    if let Some(api) = apis.get("spotify") {
+        println!("\nExecuting auth for {}", api.url);
+        let x = api.authenticate().await;
+        println!("\nResult {:?}", x);
+    }
 
-    println!("Hello apis: {:?}", &apis);
     Ok(())
 }
 
-fn fetch_apis(path_str: &str) -> Result<Vec<Api>> {
+fn fetch_apis(path_str: &str) -> Result<HashMap<String, Api>> {
     let path = Path::new(path_str);
     let contents = std::fs::read_to_string(path)?;
     let json: Value = serde_json::from_str(&contents)?;
-    let mut list: Vec<Api> = Vec::new();
+    let mut list: HashMap<String, Api> = HashMap::new();
 
     for object in json.as_array().unwrap() {
         let mut api = Api {
-            url: object["url"].to_string(),
+            url: String::from(object["url"].as_str().unwrap()),
             paths: HashMap::new(),
+            ..Default::default()
         };
 
         for (name, p) in object["paths"].as_object().unwrap() {
             let mut path = ApiPath {
-                path: p["path"].to_string(),
+                path: String::from(p["path"].as_str().unwrap()),
                 headers: http::header::HeaderMap::new(),
                 body_parameters: HashMap::new(),
+                path_parameters: HashMap::new(),
             };
 
-            for header in p["headers"].as_array().unwrap() {
-                for (key, value) in header.as_object().unwrap() {
-                    path.headers.insert(
-                        http::header::HeaderName::try_from(key.to_string()).unwrap(),
-                        http::header::HeaderValue::try_from(value.to_string()).unwrap(),
-                    );
-                }
+            for (key, value) in p["headers"].as_object().unwrap() {
+                let v = String::from(value.as_str().unwrap());
+                path.headers.insert(
+                    http::header::HeaderName::try_from(key).unwrap(),
+                    http::header::HeaderValue::try_from(v).unwrap(),
+                );
             }
 
-            for bp in p["body"].as_array().unwrap() {
-                for (key, value) in bp.as_object().unwrap() {
-                    path.body_parameters.insert(key.clone(), value.to_string());
-                }
+            for (key, value) in p["body"].as_object().unwrap() {
+                let v = String::from(value.as_str().unwrap());
+                path.body_parameters.insert(key.clone(), v);
+            }
+
+            for (key, value) in p["path-parameters"].as_object().unwrap() {
+                let v = String::from(value.as_str().unwrap());
+                path.path_parameters.insert(key.clone(), v);
             }
 
             api.paths.insert(name.clone(), path);
-            list.push(api.clone());
         }
+
+        let s_url: Vec<&str> = api.url.split(".").collect();
+        let key = s_url.get(1).unwrap_or(&"NO-DOMAIN");
+        list.insert(key.to_string(), api.clone());
     }
 
     Ok(list)
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 struct ApiPath {
     path: String,
     // #[serde(deserialize_with = "deserialize_header_map")]
     headers: http::header::HeaderMap,
     body_parameters: HashMap<String, String>,
+    path_parameters: HashMap<String, String>,
 }
 
 impl<'a> std::fmt::Display for ApiPath {
@@ -75,16 +90,111 @@ impl<'a> std::fmt::Display for ApiPath {
     }
 }
 
-#[derive(Debug, Clone)]
+impl ApiPath {
+    fn stringify_path_parametrs(&self) -> String {
+        self.path_parameters
+            .iter()
+            .map(|(k, v)| format!("{}={}{}", k, v, "&"))
+            .collect()
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+struct Authentication {
+    scope: Vec<String>,
+    valid: bool,
+    expires: i32,
+    hash: String,
+}
+
+// Accepted characters to generate random strings with
+const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+abcdefghijklmnopqrstuvwxyz\
+0123456789";
+
+impl Authentication {
+    /// Generates a random alphanumeric string of the given length.
+    ///
+    /// # Arguments
+    /// * `length` - The length of the random string (as `i32`).
+    ///
+    /// # Returns
+    /// A random alphanumeric string of the specified length.
+    ///
+    /// # Panics
+    /// Panics if `length` is negative.
+    fn generate_random_string(&self, length: i32) -> String {
+        use rand::Rng;
+        assert!(length >= 0, "Length must be non-negative");
+
+        let mut rng = rand::rng();
+        (0..length)
+            .map(|_| {
+                let idx = rng.random_range(0..CHARSET.len());
+                CHARSET[idx] as char
+            })
+            .collect()
+    }
+}
+
+#[derive(Debug, Clone, Default)]
 struct Api {
     url: String,
     paths: HashMap<String, ApiPath>,
+    authentication: Authentication,
 }
 
 impl Api {
-    async fn default_request(&self, subpath: &ApiPath) -> Result<reqwest::Response> {
+    async fn authenticate(&self) -> Result<(reqwest::Response, String)> {
+        let state = self.authentication.generate_random_string(16);
+        let mut auth_path = self.paths.get("authenticate").cloned().unwrap();
+        auth_path
+            .path_parameters
+            .insert("state".to_string(), state.clone());
+
+        let redirect = env::var("SPOTIFY_REDIRECT_URI").unwrap_or_else(|_| "NONE".to_string());
+        let client_id = env::var("SPOTIFY_CLIENT_ID").unwrap_or_else(|_| "NONE".to_string());
+
+        auth_path
+            .path_parameters
+            .entry("redirect_uri".to_string())
+            .and_modify(|v| *v = redirect.clone())
+            .or_insert(redirect);
+        auth_path
+            .path_parameters
+            .entry("client_id".to_string())
+            .and_modify(|v| *v = client_id.clone())
+            .or_insert(client_id);
+
+        println!("{:?}", self);
+        let response = self.get_request(&auth_path).await.unwrap();
+        println!("RESPONSE: {:?}", response);
+        Ok((response, state))
+    }
+
+    async fn get_request(&self, subpath: &ApiPath) -> Result<reqwest::Response> {
         let client = reqwest::Client::new();
-        let url = format!("{}{}", &self.url, subpath);
+        let url = format!(
+            "{}{}{}",
+            &self.url,
+            subpath.path,
+            subpath.stringify_path_parametrs()
+        );
+        println!("URL {:?}", url);
+        let response = client
+            .get(url)
+            .header("content-type", "application/json")
+            .headers(subpath.headers.clone())
+            .send()
+            .await?;
+
+        println!("Request response {:?}", response);
+        Ok(response)
+    }
+
+    async fn post_request(&self, subpath: &ApiPath) -> Result<reqwest::Response> {
+        let client = reqwest::Client::new();
+        let url = format!("{}{}", &self.url, subpath.path);
         let response = client
             .post(url)
             .form(&subpath.body_parameters)
