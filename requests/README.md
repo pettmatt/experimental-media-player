e9240853be33d572575ab5ff96a579c61bf8d3b8
pettmatt <petterimattila@protonmail.com>
2026-07-28 13:22:01 +0300

    Able to extract audio with yt-dlp

---
+++ requests/README.md
@@ -0,0 +1,16 @@
+# Binaries
+
+**td-dlp:**
+
+```sh
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ./libs/yt-dlp
+chmod a+rx ./libs/yt-dlp  # Make executable
+```
+
+To update:
+
+```sh
+yt-dlp -U
+```
+
+**ffmpeg:**
--- requests/src/stream/extract.rs
+++ requests/src/stream/extract.rs
@@ -1,6 +1,7 @@
+use std::fs;
 use std::os::unix::fs::PermissionsExt;
 use yt_dlp::model::playlist::Playlist;
-use yt_dlp::model::AudioQuality;
+// use yt_dlp::model::AudioQuality;
 use yt_dlp::Downloader;
 use yt_dlp::client::deps::Libraries;
 use std::path::PathBuf;
@@ -18,20 +19,33 @@ impl Extractor {
 
 		let yt = libraries_dir.join("yt-dlp");
 		let ffmpeg = libraries_dir.join("ffmpeg");
-		let _ = std::fs::create_dir_all(&yt);
-		let _ = std::fs::create_dir_all(&ffmpeg);
-		let libraries = Libraries::new(yt, ffmpeg);
 
+		// if !yt.exists() || !yt.is_file() {
+		// 	let _ = std::fs::File::create("libs/yt-dlp");
+		// }
+		// if !ffmpeg.exists() || !ffmpeg.is_file() {
+		// 	let _ = std::fs::File::create("libs/ffmpeg");
+		// }
+
+		let libraries = Libraries::new(yt.clone(), ffmpeg.clone());
 		for bin in [libraries_dir.join("yt-dlp"), libraries_dir.join("ffmpeg")] {
 			let mut permissions = tokio::fs::metadata(&bin).await?.permissions();
 			permissions.set_mode(0o755);
 			tokio::fs::set_permissions(&bin, permissions).await?;
 		}
 
-		let downloader = Downloader::builder(libraries, "output")
+		let downloader = Downloader::with_new_binaries(libraries_dir, "output")
+    		.await?
 			.with_timeout(std::time::Duration::from_secs(20))
 			.build()
 			.await?;
+
+		if !fs::metadata(&yt).is_ok() {
+			return Err("yt-dlp binary not found in libs/".into());
+		}
+		if !fs::metadata(&ffmpeg).is_ok() {
+			return Err("ffmpeg binary not found in libs/".into());
+		}
 
 		Ok(Self {
 			downloader: downloader
