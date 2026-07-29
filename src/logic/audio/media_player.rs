9ba0132c1a60cef0f72560e3bc6f89aee73251e9
pettmatt <petterimattila@protonmail.com>
2026-07-29 16:05:26 +0300

    Switched core library in media player

--- src/logic/audio/media_player.rs
+++ src/logic/audio/media_player.rs
@@ -1,176 +1,180 @@
-use super::sink::Sink;
-use crate::{logic::data_types::track::Track, State};
-// use rodio::{self, Decoder, OutputStream};
-use std::{
-    fs::File,
-    path::Path,
-    sync::{Arc, Mutex},
-    time::Duration,
-};
+use crate::logic::data_types::track::Track;
+use crate::State;
+use kira::sound::streaming::{StreamingSoundData, StreamingSoundHandle};
+use kira::sound::FromFileError;
+use kira::{AudioManager, AudioManagerSettings, DefaultBackend, Tween};
+use std::sync::Mutex;
+use std::time::Duration;
 
-// Rodio docs: https://docs.rs/rodio/latest/rodio/
-// Note: The sound plays in a separate audio thread,
-// so we need to keep the main thread alive while it's playing.
-
-pub struct MediaPlayer {
-    sink: Option<Arc<Mutex<Sink>>>,
-    // stream_handle: Option<OutputStream>,
+pub struct MediaPlayer<'a> {
+    currently: Option<&'a Track>,
+    queue: Vec<&'a Track>,
+    controls: Controls,
+    manager: Option<AudioManager<DefaultBackend>>,
+    handle: Option<StreamingSoundHandle<kira::sound::FromFileError>>,
 }
 
-impl MediaPlayer {
+struct Controls {
+    loop_pick: bool,
+    rng_pick: bool,
+    pause: bool,
+    stopped: bool,
+    volume: Mutex<f32>,
+    position: Mutex<Duration>,
+    length: Mutex<Duration>,
+}
+
+// TODO: Add a logic that loops the queue if wanted.
+// TODO: Add a logic that puts tracks from queue to "trash"-queue.
+// TODO: Add a logic picks random track (would use trash-queue to prevent to pick previous tracks).
+
+impl<'a> MediaPlayer<'a> {
     pub fn new() -> Self {
         Self {
-            sink: None,
-            // stream_handle: None,
+            currently: None,
+            queue: Vec::new(),
+            manager: None,
+            handle: None,
+            controls: Controls {
+                loop_pick: false,
+                rng_pick: false,
+                pause: false,
+                stopped: false,
+                volume: Mutex::new(1.0),
+                position: Mutex::new(Duration::new(0, 0)),
+                length: Mutex::new(Duration::new(0, 0)),
+            },
         }
     }
 
-    pub fn start(&mut self, audio: &Track) {
-        if self.sink.is_none() {
-            // let (stream_handle, new_sink) = open_stream();
-            // self.sink = Some(Arc::new(Mutex::new(new_sink)));
-            // self.stream_handle = Some(stream_handle);
+    pub async fn play(&mut self, source: &'a Track) -> Result<(), Box<dyn std::error::Error>> {
+        if self.controls.pause == false {
+            return Ok(());
         }
-
-        if let Some(guard) = &self.sink {
-            let audio_path = Path::new(&audio.path);
-            if let Ok(sink) = guard.lock() {
-                sink.set_volume(0.1);
-                // start_playing(&sink, audio_path);
-            }
-        }
-    }
-
-    pub fn start_next(&mut self, audio: &Track) {}
-
-    pub fn source_toggle(&mut self) {
-        if self.sink.is_none() {
-            return;
-        }
-
-        if let Some(guard) = &self.sink {
-            if let Ok(sink) = guard.lock() {
-                if sink.is_paused() {
-                    sink.play();
+        self.controls.pause = false;
+        if self.currently.is_some() {
+            if let Some(current) = self.currently {
+                if current != source {
+                    self.currently = Some(source);
+                    self.queue.push(source);
+                }
+                if self.manager.is_some() {
+                    if let Some(manager) = self.manager.as_mut() {
+                        let sound_data =
+                            StreamingSoundData::from_file("./requests/output/audio.mp3")?;
+                        let handle = manager.play(sound_data)?;
+                        self.handle = Some(handle);
+                    }
                 } else {
-                    sink.pause();
+                    let mut manager =
+                        AudioManager::<DefaultBackend>::new(AudioManagerSettings::default())?;
+                    let sound_data = StreamingSoundData::from_file("./requests/output/audio.mp3")?;
+                    self.controls.length = Mutex::new(self.get_track_length(&sound_data));
+                    let mut handle = manager.play(sound_data)?;
+                    handle.set_volume(kira::Decibels(-3.0), Tween::default());
+                    self.manager = Some(manager);
+                    self.handle = Some(handle);
                 }
             }
+        }
+        Ok(())
+    }
+
+    pub fn unpause(&mut self) {
+        if let Some(handle) = self.handle.as_mut() {
+            self.controls.pause = false;
+            handle.resume(Tween::default());
         }
     }
 
     pub fn pause(&mut self) {
-        if let Some(guard) = &self.sink {
-            if let Ok(sink) = guard.lock() {
-                if !sink.is_paused() {
-                    sink.pause();
+        if let Some(handle) = self.handle.as_mut() {
+            self.controls.pause = true;
+            handle.pause(Tween::default());
+        }
+    }
+
+    pub fn is_paused(&self) -> bool {
+        self.controls.pause
+    }
+
+    pub fn is_loop(&self) -> bool {
+        self.controls.loop_pick
+    }
+
+    pub fn is_rng(&self) -> bool {
+        self.controls.rng_pick
+    }
+
+    pub fn set_queue_loop(&mut self, b: bool) {
+        self.controls.loop_pick = b;
+        self.controls.rng_pick = false;
+    }
+
+    pub fn set_queue_rng(&mut self, b: bool) {
+        self.controls.rng_pick = b;
+        self.controls.loop_pick = false;
+    }
+
+    pub fn stop(&mut self) {
+        if let Some(handle) = self.handle.as_mut() {
+            handle.stop(Tween::default());
+        }
+    }
+
+    pub fn play_specific_in_queue(&mut self, index: i32) {}
+
+    pub fn get_track_length(&self, track_data: &StreamingSoundData<FromFileError>) -> Duration {
+        track_data.duration()
+    }
+
+    pub fn clear(&mut self) {
+        self.currently = None;
+        self.queue = Vec::new();
+        self.controls.position = Mutex::new(Duration::new(0, 0));
+        self.manager = None;
+        self.handle = None;
+    }
+
+    pub fn volume(&self) -> f32 {
+        *self.controls.volume.lock().unwrap()
+    }
+
+    pub fn set_volume(&self, value: f32) {
+        *self.controls.volume.lock().unwrap() = value;
+    }
+
+    // Checks if the track (item) is in queue and returns false if it is.
+    // This can be used to prevent user from adding multiple same tracks.
+    pub fn append(&mut self, track: &'a Track, i_know: bool) -> bool {
+        if i_know == false {
+            for item in self.queue.iter() {
+                if item == &track {
+                    return false;
                 }
             }
         }
+        self.queue.push(track);
+        true
     }
 
-    pub fn source_change_to_specific(&mut self, index: i32) {
-        if let Some(sink) = &mut self.sink {
-            // *sink.detach();
-        }
-    }
-
-    pub fn next(&self) {
-        if let Some(guard) = &self.sink {
-            if let Ok(sink) = guard.lock() {
-                sink.skip_one();
-            }
-        }
-    }
-
-    pub fn previous(&mut self, state: &mut State) {
-        if let Some(sink) = &self.sink {
-            // let current_audio: Vec<QueueItem> = state.queue
-            // 	.clone()
-            // 	.into_iter()
-            // 	.filter(|item| item.playing == false)
-            // 	.collect();
-
-            // let mut previous_audio = None;
-
-            // let mut index: i32 = 0;
-            // for audio in state.index.iter() {
-            // 	if audio.id == current_audio[0].media_id {
-            // 		break;
-            // 	}
-
-            // 	index += 1;
-            // }
-
-            // if let Some(audio) = previous_audio {
-            // 	// Destroy current queue
-            // 	self.destroy_sink();
-            // 	self.new_sink();
-            // 	self.add_to_queue(state, audio.clone()); // Add the previous track to queue.
-            // 	sink.play();
-
-            // 	self.load_queue_from_state(state);
-            // }
-
-            // sink.append(source);
-            // update_playing(state, -1);
-        }
-    }
-
-    pub fn clear_queue(&self) {
-        if let Some(stream_handle) = &self.stream_handle {
-            // stream_handle.cl
-        }
-    }
-
-    pub fn create_queue(&self, media_files: Vec<&Track>) -> Result<(), ()> {
-        let _ = media_files.iter().map(|media_file| {
-            if let Some(sink) = &self.sink {
-                {
-                    let guard = sink.lock().unwrap();
-                    guard.clear();
-                }
-
-                let file = File::open(&media_file.path).unwrap();
-                // let source = Decoder::try_from(file).unwrap();
-
-                if let Some(guard) = self.sink.as_ref() {
-                    if let Ok(sink) = guard.lock() {
-                        // sink.append(source);
-                    }
+    pub fn next(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        if self.queue.len() > 0 {
+            let track: &Track = self.queue.remove(0);
+            if let Some(manager) = self.manager.as_mut() {
+                self.currently = Some(track);
+                if let Some(current) = self.currently {
+                    let data = StreamingSoundData::from_file(current.path.clone())?;
+                    self.handle = Some(manager.play(data)?);
                 }
             }
-        });
-
+        }
         Ok(())
     }
 
-    pub fn add_to_queue(&self, state: &mut State, media_file: &Track) -> Result<(), ()> {
-        if let Ok(file) = File::open(&media_file.path) {
-            // if let Ok(source) = Decoder::try_from(file) {
-            //     if let Some(guard) = &self.sink {
-            //         if let Ok(sink) = guard.lock() {
-            //             sink.append(source);
-            //             // state.queue.push(QueueItem {
-            //             // 	media_id: media_file.id,
-            //             // 	playing: false,
-            //             // });
+    pub fn previous(&mut self, state: &mut State) {}
 
-            //             return Ok(());
-            //         }
-            //     }
-            // } else {
-            //     println!("Can't add media to queue. Couldn't convert file.");
-            // }
-        } else {
-            println!(
-                "Can't add media to queue. Path might be unvalid: {}",
-                media_file.path
-            );
-        }
-
-        Err(())
-    }
+    pub fn clear_queue(&self) {}
 
     fn load_queue_from_state(&mut self, state: &State) {
         // self.queue = state.queue.clone();
@@ -182,76 +186,25 @@ impl MediaPlayer {
         // }
     }
 
-    pub fn change_position(&mut self, position: Duration) {
-        if let Some(guard) = &self.sink {
-            if let Ok(sink) = guard.lock() {
-                let _ = sink.try_seek(position);
-            }
+    pub fn try_seek(&mut self, position: f64) {
+        if let Some(handle) = self.handle.as_mut() {
+            handle.seek_to(position);
         }
     }
 
-    pub fn get_position(&self) -> u32 {
-        if let Some(guard) = &self.sink {
-            if let Ok(sink) = guard.lock() {
-                return sink.get_pos().as_secs_f32() as u32;
-            }
-        }
-
-        0
-    }
-
-    pub fn set_volume(&self, value: f32) {
-        if let Some(sink) = &self.sink {
-            if let Ok(guard) = sink.lock() {
-                guard.set_volume(value);
-            }
+    pub fn try_seek_by(&mut self, amount: f64) {
+        if let Some(handle) = self.handle.as_mut() {
+            handle.seek_by(amount);
         }
     }
 
-    pub async fn callback_after_audio_ends(&self, callback: fn()) {
-        if let Some(guard) = &self.sink {
-            let temp = guard.clone();
-            let (sender, receiver) = std::sync::mpsc::channel();
-
-            std::thread::spawn(move || {
-                if let Ok(sink) = temp.lock() {
-                    sender
-                        .send("(Audio) Executing after audio ends callback")
-                        .unwrap();
-                }
-            });
-
-            if let Ok(message) = receiver.recv() {
-                println!("(Message) received");
-                println!("{}", message);
-                callback();
-                // self.callback_after_audio_ends(callback);
-            }
+    pub fn get_position(&mut self) -> f64 {
+        if let Some(handle) = self.handle.as_mut() {
+            return handle.position();
         }
+
+        0.0
     }
-}
 
-// fn open_stream() -> (OutputStream, Sink) {
-    // let stream_handle =
-    //     rodio::OutputStreamBuilder::open_default_stream().expect("Open default audio stream");
-    // let sink = Sink::connect_new(stream_handle.mixer());
-    // (stream_handle, sink)
-// }
-
-fn start_playing_from_stream() {}
-
-fn start_playing_local_audio(sink: &Sink, audio_path: &Path) {
-    // if let Ok(file) = File::open(audio_path) {
-    //     match Decoder::try_from(file) {
-    //         Ok(source) => {
-    //             sink.append(source);
-    //             sink.play();
-    //         }
-    //         Err(error) => {
-    //             println!("(Audio) - start_playing_audio DecoderError: {:?}", error);
-    //         }
-    //     }
-    // } else {
-    //     println!("Couldn't open audio file: {:?}", audio_path);
-    // }
+    pub async fn callback_after_audio_ends(&self, callback: fn()) {}
 }
